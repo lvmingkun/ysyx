3712e0d0dac45434ebba001ae4c184442fe93ade
tracer-ysyx <tracer@ysyx.org>
2025-01-12 12:07:12 +0800

    > sim RTL ysyx_22040000 方明杰 Linux kkcat-ucas-ict 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:07:12 up  2:06,  1 user,  load average: 0.39, 0.39, 0.42

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -3,20 +3,30 @@
 #include <assert.h>
 #include "Vtop.h"
 #include "verilated.h"
+#include "verilated_vcd_c.h"
 
 int main(int argc, char** argv) {
 	VerilatedContext* contextp = new VerilatedContext;
 	contextp->commandArgs(argc, argv);
 	Vtop* top = new Vtop{contextp};
+	contextp->traceEverOn(true);
+	VerilatedVcdC* tfp = new VerilatedVcdC;
+	top->trace(tfp, 99);
+	tfp->open("wave.fst");
 	while (true) {
 		int a = rand() & 1;
 		int b = rand() & 1;
 		top->a = a;
 		top->b = b;
 		top->eval();
+		contextp->timeInc(1);
+		if(tfp) tfp->dump(contextp->time());
 		printf("a = %d, b = %d, f = %d\n", a, b, top->f);
 		assert(top->f == (a ^ b));
 	}
+
+	tfp->close();
+	delete tfp;
 	delete top;
 	delete contextp;
   return 0;
