fc9eb3df25906797b3efcfa1f236050e06a7ff7a
tracer-ysyx <tracer@ysyx.org>
2025-01-16 21:09:01 +0800

    > compile NEMU ysyx_22040000 方明杰 Linux kkcat-ucas-ict 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:09:01 up 11:19,  3 users,  load average: 0.14, 0.20, 0.26

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -53,7 +53,7 @@ int main(int argc, char *argv[]) {
 			bool success = false;
 			word_t valid_value = expr(expre, &success);
 			if (success && valid_value == atoi(value)) {
-				// printf("The %d expression %s calculate successfully\n", success_num, expre);
+				printf("The %d expression %s calculate successfully\n", success_num, expre);
 				success_num++;
 			} else {
 				printf("The expression %s calculate unsuccessfully\n", expre);
