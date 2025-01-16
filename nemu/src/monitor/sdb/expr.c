d87679c34d93b24c31f6f2e16e5378bf3379e78e
tracer-ysyx <tracer@ysyx.org>
2025-01-16 21:50:59 +0800

    > compile NEMU ysyx_22040000 方明杰 Linux kkcat-ucas-ict 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:50:59 up 12:01,  1 user,  load average: 0.53, 0.38, 0.34

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -160,7 +160,7 @@ bool check_parentheses(int p, int q, bool *qs) {
 
 int find_op(int p, int q) {
 	int t = p + 1;
-	bool par = false;
+	int par = 0;
 	int precedence[272] = {0};
 	// printf("%d  %d\n", t, op);
 
@@ -170,10 +170,10 @@ int find_op(int p, int q) {
 	precedence['/'] = 1;
 
 	while (t <= q) {
-		if (tokens[t].type == '(') par = true;
-		if (tokens[t].type == ')') par = false;
+		if (tokens[t].type == '(') par++;
+		if (tokens[t].type == ')') par--;
 
-		if (!par && precedence[tokens[p].type] <= precedence[tokens[t].type]){
+		if (par == 0 && precedence[tokens[p].type] <= precedence[tokens[t].type]){
 			p = t;
 			// printf("%d  %d  %d\n", p, t, q);
 		}
