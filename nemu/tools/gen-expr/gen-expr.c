88f8191a7a9dcd4a5c689404dc1987a5b650effd
tracer-ysyx <tracer@ysyx.org>
2025-01-16 18:11:18 +0800

    > compile NEMU ysyx_22040000 方明杰 Linux kkcat-ucas-ict 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:11:18 up  8:22,  3 users,  load average: 1.02, 0.59, 0.39

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -16,9 +16,14 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <time.h>
 #include <assert.h>
 #include <string.h>
+
+#define MAX_BUF 65534
+#define MAX_PAR 3
+#define MAX_REC 5
 
 // this should be enough
 static char buf[65536] = {};
@@ -31,12 +36,108 @@ static char *code_format =
 "  return 0; "
 "}";
 
+static int flags = 0;
+static int parnum = 0;
+static int renum = 0;
+static bool zflag = false;
+static char nonnull = 'a';
+
+uint32_t choose(uint32_t n) {
+	uint32_t random_num = (uint32_t) (rand() % n);
+	printf("rec %d choose %d\n", renum, random_num);
+	return random_num;
+}
+
+static void gen_num() {
+	if (nonnull == ')') {
+		printf("rec %d, have ) can't gen num, return!\n", renum);
+		return;
+	}
+	int len = rand() % 4 + 1;
+	if (flags < MAX_BUF - len - 1) {
+		for (int i = 0; i < len; i++) {
+			char random_char = (rand() % 10) + '0';
+			if ((len > 1) && (i == 0) && (random_char == '0')) {
+				random_char = '1';
+				printf("0 head,it will add 1 to be %c\n", random_char);
+			}
+			if (zflag && len == 1 && random_char == '0') {
+				random_char = '1';
+				printf(" gen 0 but dev 0,add 1 to be %c\n", random_char);
+			}	
+			buf[flags] = random_char;
+			nonnull = buf[flags];
+			printf("rec %d, gen num %c\n", renum, buf[flags]);
+			flags++;
+			zflag = false;
+		}
+	}
+	else {
+		printf("rec %d, overflow\n", renum);
+		return;
+	}
+}
+
+static void gen(char par) {
+	if (((nonnull == ')' || parnum >= MAX_PAR || flags >= MAX_BUF - 10) && par == '(') || (par == ')' && parnum <= 0)) {
+		printf("rec %d want to gen (  or ) but parnum is %d or have ),return\n",renum, parnum);
+		return;
+	}
+	else {
+		if (flags < MAX_BUF) {
+			if (par == '(') parnum++;
+			else parnum--;
+			buf[flags] = par;
+			nonnull = buf[flags];
+			printf("rec %d gen %c\n", renum, buf[flags]);
+			flags++;
+		}
+		else {
+			printf("rec %d overflow, return\n", renum);
+			return;
+		}
+	}
+}
+
+static void gen_rand_op() {
+	char sign[4] = {'+', '-', '*', '/'};
+	if (flags > 0 && flags < MAX_BUF - 10) {
+		buf[flags] = sign[rand() % 4];
+		nonnull = buf[flags];
+		printf("rec %d gen op %c\n", renum, buf[flags]);
+		if (buf[flags] == '/') zflag = true;
+		flags++;
+	}
+	else {
+		printf("rec %d want to gen op but overflow or null\n", renum);
+		return;
+	}
+}
+
 static void gen_rand_expr() {
-  buf[0] = '\0';
+	renum++;
+	if (choose(6) == 1 && flags < MAX_BUF) {
+			buf[flags] = ' ';
+			printf("gen %c\n", buf[flags]);
+		  flags++;
+	}
+	else printf("so no gen     \n");
+	if (renum >= MAX_REC) {
+		printf("rec has max %d\n", renum);
+		if (nonnull < '0' || nonnull > '9') gen_num();
+		else return;
+	}
+	else {
+    switch (choose(3)) {
+			case 0: gen_num(); break;
+			case 1: gen('('); gen_rand_expr(); gen(')'); break;
+			default: gen_rand_expr(); gen_rand_op(); gen_rand_expr(); break;
+	  }
+	}
 }
 
 int main(int argc, char *argv[]) {
-  int seed = time(0);
+  int seed = time(NULL);
   srand(seed);
   int loop = 1;
   if (argc > 1) {
@@ -45,7 +146,7 @@ int main(int argc, char *argv[]) {
   int i;
   for (i = 0; i < loop; i ++) {
     gen_rand_expr();
-
+    buf[flags] = '\0';
     sprintf(code_buf, code_format, buf);
 
     FILE *fp = fopen("/tmp/.code.c", "w");
@@ -53,7 +154,7 @@ int main(int argc, char *argv[]) {
     fputs(code_buf, fp);
     fclose(fp);
 
-    int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
+    int ret = system("gcc -Wall -Werror /tmp/.code.c -o /tmp/.expr");
     if (ret != 0) continue;
 
     fp = popen("/tmp/.expr", "r");
@@ -64,6 +165,12 @@ int main(int argc, char *argv[]) {
     pclose(fp);
 
     printf("%u %s\n", result, buf);
+		memset(buf, 0, sizeof(buf));
+		memset(code_buf, 0, sizeof(code_buf));
+		renum = 0;
+		flags = 0;
+		zflag = false;
+		nonnull = 'a';
   }
   return 0;
 }
